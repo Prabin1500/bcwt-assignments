561a1bf195d096062c2ef3fa86d4c319ee4eb0a1
prabind <prabin.dhakal@metropolia.fi>
2022-11-02 10:49:36 +0200

    added delete feature to delete user password

--- week-two/controllers/userController.js
+++ week-two/controllers/userController.js
@@ -7,13 +7,20 @@ const userModel = require('../models/userModel');
 const users = userModel.users;
 
 const getUsers = (req, res) => {
+    users.map(user =>{
+        delete user.password;
+        return user;
+    });
+
     res.json(users);
 };
 
 const getUser = (req,res) => {
+    //choose only one user with matching id
     const user = users.filter(user => req.params.userId == user.id)[0];
 
     if(user){
+        delete user.password;
         res.json(user);
     }else{
         res.sendStatus(404);
