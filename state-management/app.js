96ccbd14c41c0db97c50185b75fe23627eaed6f5
prabind <prabin.dhakal@metropolia.fi>
2022-11-23 17:33:43 +0200

    Session task done

--- state-management/app.js
+++ state-management/app.js
@@ -1,16 +1,59 @@
 'use strict';
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const app = express();
 const port = 3000;
+
+const user = {
+  username: 'foo',
+  password: 'bar'
+};
+
+//dont do this
+let loggedIn = false;
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(cookieParser());
+app.use(express.json());   //for parsing application 
+app.use(express.urlencoded({extended: true}));   //for form data
+app.use(session({
+  secret: 'kjsdaöklfnajsadfg34ttVAga',
+  saveUninitialized: false,
+  resave: true,
+  cookie: {maxAge: 60000}
+}))
 
 app.get('/', (req, res) => {
   res.render('home');
 });
+
+app.get('/form', (req, res) => {
+  res.render('form');
+});
+
+app.get('/secret', (req, res) => {
+  if(req.session.loggedIn){
+    res.render('secret');
+  }else{
+    res.redirect('/form');
+  }
+});
+
+app.post('/login', (req,res) =>{
+  console.log(req.body);
+  if(req.body.username == user.username && req.body.password == user.password){
+    req.session.loggedIn = true;
+  }
+  res.redirect('/secret');
+  
+});
+
+app.get('/logout', (req, res) => {
+  req.session.loggedIn = false;
+  res.redirect('/');
+})
 
 app.get('/getCookie', (req, res) => {
   console.log(req.cookies);
